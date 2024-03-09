91be1ea873765895799282eeb59bc9a3801d6f31
SHIFA FATIMA <shifa.fatimeh@gmail.com>
2024-03-09 23:06:50 +0530

    debug

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-let bagesd = document.querySelector(".badge");
-let whole = document.querySelector(".card-1-one");
-whole.addEventListener("mouseover", function () {
-  bagesd.classList.remove("badgee");
+let badge = document.querySelectorAll(".badge");
+let whole = document.querySelectorAll(".card-1-one");
+let cards = document.querySelector(".cards");
+// console.log(badge);
+// console.log(whole);
+
+cards.addEventListener("mouseover", function (e) {
+  console.log(e.target.classList);
+  if (e.target.classList.contains("badge")) {
+    console.log(e.target.className);
+    // badge.classList.remove("badgee");
+    e.target.className.replace("badgee", "");
+  }
 });
-whole.addEventListener("mouseout", function () {
-  bagesd.classList.add("badgee");
+cards.addEventListener("mouseout", function (e) {
+  if (e.target.classList.contains("badge")) {
+    // badge.classList.remove("badgee");
+    e.target.className += " badgee";
+  }
 });
-̀
+
+// if (badge.classList.contains("transition-ease")) {
+//     badge.classList.remove("transition-ease");
+//     badge.classList.add("transition-steps");
+//   } else {
+//     badge.classList.remove("transition-steps");
+//     badge.classList.add("transition-ease");
+//   }
+// }
